bca45bdf4925eea180592539c8cf2909e11290b2
Adan Mendez Lemus <adanmendezlemus@gmail.com>
2013-12-18 00:37:19 +0000

    Documentacion de Arista

--- src/main/java/algorithm/Arista.java
+++ src/main/java/algorithm/Arista.java
@@ -1,20 +1,39 @@
 package algorithm;
 
+/**
+ * 
+ * @author Adán Méndez
+ * Esta clase guarda la informacion de las aristas
+ */
 public class Arista {
 	int from;		//Nodo de origen
 	int to;			//Nodo destino
 	double cost;	//Costo de usar la arista
 	
+	/**
+	 * Constructor
+	 * @param from Nodo origen
+	 * @param to Nodo destino
+	 * @param cost Coste de la arista
+	 */
 	public Arista( int from, int to, double cost ){
 		this.from = from;
 		this.to = to;
 		this.cost = cost;
 	}
 	
+	/**
+	 * 
+	 * @return to Devuelve el nodo destino
+	 */
 	public int getTo(){
 		return this.to;
 	}
 	
+	/**
+	 * 
+	 * @return devuelve el costo
+	 */
 	public double getCost(){
 		return this.cost;
 	}
